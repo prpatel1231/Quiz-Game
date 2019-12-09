7fe2897a90f91865071d54c097c26bf99f82fd7d
prpatel <prpatel1231@gmail.com>
2019-12-09 07:47:41 -0500

    added reset game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,13 @@ var highScoreBtn = document.getElementById("highScores");
 var timer = document.getElementById("timer");
 var questionContainer = document.getElementById("question");
 var answerContainer = document.getElementById("answers");
+var startContainer = document.getElementById("start")
 var startBtn = document.getElementById("startBtn");
 var resultElement = document.getElementById("result");
 
 //initialize global variables for the game
 var score = 0;
-var time = 15;
-var questionsLeft = 0;
+var time = 15 * questions.length;
 var questionIndex = 0;
 
 //create array of objects that houses the questions and answers
@@ -31,56 +31,61 @@ var questions = [
 startBtn.addEventListener("click", function() {
     //reset game variables
     score = 0;
-    time = 15;
-    questionsLeft = questions.length - 1;
+    time = 15 * questions.length;
     questionIndex = 0;
-    //set first question of the quiz
-    var questionTitle = questions[questionIndex].title;
-    questionContainer.innerText = questionTitle;
     //remove the start button
-    answerContainer.removeChild(startBtn);
+    startContainer.removeChild(startBtn);
     createQuestion();
 });
     
 function createQuestion(){
+    if (questionIndex > questions.length){
+        return false
+    };
+
+    //set question of the quiz
+    var questionTitle = questions[questionIndex].title;
+    questionContainer.innerText = questionTitle;
+
     //loop through th answer choices and dynamically create buttons for the respective choices
     var answerChoices = questions[questionIndex].choices;
-    for (i = 0; i <= answerChoices.length; i++) {
+    answerContainer.innerHTML = "";
+    for (i = 0; i < answerChoices.length; i++) {
         var answerBtn = document.createElement("button");
         answerBtn.setAttribute("class", "btn btn-dark answer");
         answerBtn.innerText = answerChoices[i];
         answerContainer.appendChild(answerBtn);
     };
+
+ 
 };
-
-
 //event function to check whether the clicked answer button is correct, update score and move to next question
 //use event delegation to create an event listener over all dynamically created answer buttons
 
 answerContainer.addEventListener("click", function(event){
     var guess = event.target.innerText;
     correctAnswer = questions[questionIndex].answer;
-
-    //if no more questions are left then exist
-    if (questionsLeft = 0){
-        return false
-    }
     //check if guess = answer, update score and increment to next question
     if (guess === correctAnswer){
         score++;
-        questionIndex++;
-        questionsLeft--;
+        questionIndex++
         resultElement.textContent = "Correct!";
         createQuestion();
     }
     else {
         questionIndex++;
-        questionsLeft--;
         resultElement.textContent = "Wrong!";
-        createQuestion()
+        createQuestion();
     }
-    
-})
+});
+
+//function to reset the game after the last question is answered or time run outs 
+function resetQuiz(){
+    questionContainer.innerText = "Click Below to Start the Quiz";
+    answerContainer.innerHTML = "";
+    startContainer.appendChild(startBtn);
+
+}
 
 
 
